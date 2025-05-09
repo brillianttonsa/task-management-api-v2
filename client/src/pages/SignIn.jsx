daf5d3bf3421192ea465604150ed3ac20a12b6db
Abdullatif Mnyamisi <abdullatifmnyamis@gmail.com>
2025-05-09 02:26:11 -0700

    home page setting up

---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+
+
+function SignIn(){
+    return(
+        <>
+            <h1>hellow</h1>
+        </>
+    )
+}
+
+export default SignIn
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+
+function SignUp(){
+    return(
+        <>
+        <h2>theer is a good girl</h2></>
+    )
+}
+
+export default SignUp
