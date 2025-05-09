79d43ae3554a16c3a7efa27eb92b7c5ba0f2f33f
Abdullatif Mnyamisi <abdullatifmnyamis@gmail.com>
2025-05-09 12:16:30 -0700

    setting signup

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,14 @@
-
+import { useState } from "react"
+import { Eye, EyeOff, XCircle } from "lucide-react"
+import { Link } from "react-router-dom"
 
 function SignIn(){
     return(
         <>
-            <h1>hellow</h1>
+           <div class="min-h-screen bg-blue-500">
+  <h1>Hello, World!</h1>
+  <p>This div will take up at least the height of the screen.</p>
+</div>
         </>
     )
 }
